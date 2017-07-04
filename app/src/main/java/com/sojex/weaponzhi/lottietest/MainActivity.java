a394b8de8436f86c43f191c2b0a3be691529f966
WeaponZhi <584098488@qq.com>
2017-07-04 18:07:52 +0800

    测试 Lottie 内存GC

--- app/src/main/java/com/sojex/weaponzhi/lottietest/MainActivity.java
+++ app/src/main/java/com/sojex/weaponzhi/lottietest/MainActivity.java
@@ -4,46 +4,81 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.airbnb.lottie.LottieAnimationView;
 
+
 public class MainActivity extends AppCompatActivity {
-    private LottieAnimationView mLottieAnimationView;
-    private boolean isPlay;
+    private LottieAnimationView mLottieAnimationView, mLottieAnimationView1;
+    private Button mButton, mButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLottieAnimationView = (LottieAnimationView) findViewById(R.id.lav_test);
-        mLottieAnimationView.setImageAssetsFolder("images/");
-        mLottieAnimationView.useExperimentalHardwareAcceleration(true);
-        mLottieAnimationView.setAnimation("data.json");
-        mLottieAnimationView.loop(true);
-        mLottieAnimationView.playAnimation();
-//        mLottieAnimationView.enableMergePathsForKitKatAndAbove(true);
 
-        mLottieAnimationView.setOnClickListener(new View.OnClickListener() {
+        mButton = (Button) findViewById(R.id.btn);
+        mButton1 = (Button) findViewById(R.id.btn_right);
+
+        mLottieAnimationView = (LottieAnimationView) findViewById(R.id.lav_test);
+        mLottieAnimationView1 = (LottieAnimationView) findViewById(R.id.lav_test_right);
+
+        mLottieAnimationView.loop(true);
+        mLottieAnimationView1.loop(true);
+        mLottieAnimationView1.setImageAssetsFolder("images3/");
+        mLottieAnimationView1.useExperimentalHardwareAcceleration(true);
+        mLottieAnimationView1.setAnimation("caishen.json");
+        mLottieAnimationView.setImageAssetsFolder("images2/");
+        mLottieAnimationView.useExperimentalHardwareAcceleration(true);
+        mLottieAnimationView.setAnimation("text2.json", LottieAnimationView.CacheStrategy.Weak);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TestActivity.class));
+                if (mLottieAnimationView.getVisibility() == View.VISIBLE) {
+                    mLottieAnimationView.setVisibility(View.GONE);
+                    mLottieAnimationView.cancelAnimation();
+                } else {
+                    mLottieAnimationView.playAnimation();
+                    mLottieAnimationView.setVisibility(View.VISIBLE);
+                }
             }
         });
 
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mLottieAnimationView1.getVisibility() == View.VISIBLE) {
+                    mLottieAnimationView1.setVisibility(View.GONE);
+                    mLottieAnimationView1.cancelAnimation();
+                } else {
+                    mLottieAnimationView1.playAnimation();
+                    mLottieAnimationView1.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
     protected void onResume() {
-        mLottieAnimationView.playAnimation();
         super.onResume();
     }
 
-    /**
-     * 测试 lo
-     */
     @Override
-    protected void onStop() {
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void nextActivity(View view){
+        startActivity(new Intent(MainActivity.this,TestActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
         mLottieAnimationView.cancelAnimation();
-        super.onStop();
+        mLottieAnimationView1.cancelAnimation();
     }
 }
--- app/src/main/java/com/sojex/weaponzhi/lottietest/TestActivity.java
+++ app/src/main/java/com/sojex/weaponzhi/lottietest/TestActivity.java
@@ -1,18 +1,56 @@
 package com.sojex.weaponzhi.lottietest;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 /**
  * Created by admin on 2017/7/4.
  */
 
-public class TestActivity extends AppCompatActivity{
+public class TestActivity extends AppCompatActivity {
+    private LottieAnimationView mLottieAnimationView;
+    private boolean isPlay;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        mLottieAnimationView = (LottieAnimationView) findViewById(R.id.lav_test1);
+        mLottieAnimationView.setImageAssetsFolder("images2/");
+        mLottieAnimationView.useExperimentalHardwareAcceleration(true);
+        mLottieAnimationView.setAnimation("text2.json", LottieAnimationView.CacheStrategy.Weak);
+        mLottieAnimationView.loop(true);
+
+
+//        mLottieAnimationView.enableMergePathsForKitKatAndAbove(true);
+
+        mLottieAnimationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TestActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        mLottieAnimationView.resumeAnimation();
+        super.onResume();
+    }
+
+    /**
+     * 测试 Lottie 的内存回收机制
+     */
+
+    @Override
+    protected void onDestroy() {
+        mLottieAnimationView.cancelAnimation();
+        super.onDestroy();
     }
 }
